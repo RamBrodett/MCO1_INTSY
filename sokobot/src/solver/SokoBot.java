2d5888bbcce8028a6fd6bdadb34b44659697f240
Ram Brodett <rmbrodett@gmail.com>
2023-10-02 16:54:03 +0800

    Added tracker of walls pseudo code

--- sokobot/src/solver/SokoBot.java
+++ sokobot/src/solver/SokoBot.java
@@ -1,11 +1,36 @@
 package solver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SokoBot {
 
+  // generate a frontier,, added queue at the mean time for bfs algo?
   QueueFrontier qf;
+  // track wall?? to have a list of constraint of possible moves
+  // --Maybe boolean : True if wall and false if not
+  // 2d list for the map
+  List<List<Boolean>> walls;
 
   public SokoBot(){
     qf = new QueueFrontier();
+    walls = new ArrayList<>();
+
+    // ROUGH PSEUDO CODE
+    // read the map file
+    // get height and width first??
+    //set a nested for loop
+    /*
+        for i to range(height):
+          -List<Boolean> row = new ArrayList<>();
+
+          -for j to range(width):
+              -- define here an if else statement
+              --that if at this coordinate this character is not a wall then
+              --append FALSE in row, TRUE otherwise
+
+          -append row to walls
+     */
 
   }
 
